25babecf4f068fac67122ed34df1046b905349ec
J.T <gisfromscratch@live.de>
2015-10-04 01:29:54 +0200

    Added a VTK project refering to the header files

---
+++ Data Visualizer Samples/Earthquake VTK Sample/main.cpp
@@ -0,0 +1,22 @@
+#include <Windows.h>
+
+#include <vtkVersion.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderWindow.h>
+#include <vtkSmartPointer.h>
+#include <vtkChartXY.h>
+#include <vtkTable.h>
+#include <vtkPlot.h>
+#include <vtkFloatArray.h>
+#include <vtkContextView.h>
+#include <vtkContextScene.h>
+#include <vtkPen.h>
+
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	return 0;
+}
