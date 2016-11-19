46cea66bee74a4812810aa7094fa7e52ed4ec01a
J.T <gisfromscratch@live.de>
2016-11-19 19:12:56 +0100

    Simple view model defined using XAML for the view

---
+++ Blend Samples/WpfButtonApp/ImportTextfileApp/ViewModel/ImportTextFileViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ImportTextfileApp.Data;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace ImportTextfileApp.ViewModel
+{
+    /// <summary>
+    /// Represents the import text file view model.
+    /// </summary>
+    public class ImportTextFileViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ImportTextFileViewModel()
+        {
+            TextEncodings = new ObservableCollection<TextEncodingItem>();
+            TextEncodings.Add(new TextEncodingItem { Name = @"ASCII" });
+            TextEncodings.Add(new TextEncodingItem { Name = @"UTF-8" });
+        }
+
+        public ObservableCollection<TextEncodingItem> TextEncodings { get; set; }
+    }
+}
